f6f71598368603fc7c7de2c360bbdf810bcb48e9
Andrew Docherty <docherty@gmail.com>
2025-01-16 15:28:23 +1100

    Added initial multi-page code. Still doesn't work yet.

---
+++ ai-project-breakdown/page2.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+
+st.write("Now on page 2")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,66 @@
 import streamlit as st
-from openai import OpenAI
 
-# Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
+OPENROUTER_MODEL_LIST = ["llama3-groq-tool-use", "gemma2", "llama3", "llama3.1"]
+
+# This must be the first Streamlit command in the script.
+st.set_page_config(
+    page_title="AD's streamlit test",
+    page_icon=":material/bug_report:",
 )
 
-# Ask user for their OpenAI API key via `st.text_input`.
-# Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
-# via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
+
+def set_state(i: int):
+    st.session_state["stage"] = i
+
+
+# Using the staging pattern documented here
+# https://docs.streamlit.io/develop/concepts/design/buttons#buttons-to-continue-or-control-stages-of-a-process
+
+if "stage" not in st.session_state:
+    set_state(0)
+
+if "model_list" not in st.session_state:
+    st.session_state["model_list"] = OPENROUTER_MODEL_LIST
+
+# Main page - these are shown on every page
+st.title("AI Project Breakdown")
+
+# Setup sidebar with parameters
+with st.sidebar:
+    st.subheader("API Keys")
+    openai_api_key = st.text_input("OpenRouter API Key", type="password")
+    notion_api_key = st.text_input("Notion API Key (optional)", type="password")
+
 if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+    st.info("Please add your OpenRouter API key to continue.", icon="🗝️")
+    st.stop()
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+# All states: Show form with model parameters and input text
+if st.session_state.stage >= 0:
+    with st.form("project_description_form"):
+        model_choice = st.selectbox("Model", OPENROUTER_MODEL_LIST)
+        text_input = st.text_area("Project description", key="input_text", height=200)
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
-
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
+        generate_button = st.form_submit_button(
+            "Go", on_click=st.switch_page, args=("ai-project-breakdown/page1.py",)
         )
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state["form_main"] = dict(
+        model_choice=model_choice, text_input=text_input
+    )
+    st.write("End of stage 0")
+
+
+# Set up pages
+generate_page = st.Page(
+    "ai-project-breakdown/page1.py",
+    title="Generate Project",
+    icon=":material/handyman:",
+)
+feedback_page = st.Page(
+    "ai-project-breakdown/page2.py",
+    title="Critique Project",
+    icon=":material/handyman:",
+)
+pg = st.navigation({"Pages": [generate_page, feedback_page]})
+pg.run()
